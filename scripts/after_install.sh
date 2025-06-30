19c44f2b6192658faedcc2cc2a8a667db5bca91c
JWJung-99 <39busy@naver.com>
2025-06-30 19:15:56 +0900

    Deploy: nvm 초기화 로직 추가

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 echo 'run after_install.sh: ' >> /home/ubuntu/Test-Server/deploy.log
 
 echo 'cd /home/ubuntu/Test-Server' >> /home/ubuntu/Test-Server/deploy.log
 cd /home/ubuntu/Test-Server >> /home/ubuntu/Test-Server/deploy.log
+
+
+echo 'Current directory: ' >> /home/ubuntu/Test-Server/deploy.log
+pwd >> /home/ubuntu/Test-Server/deploy.log
+
+echo 'check nvm version: ' >> /home/ubuntu/Test-Server/deploy.log 
+npm -v
 
 echo 'npm install' >> /home/ubuntu/Test-Server/deploy.log 
 npm install >> /home/ubuntu/Test-Server/deploy.log
