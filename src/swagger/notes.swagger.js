b948962d7e1a090dd9372b4325bccaf9443a5a53
JWJung-99 <39busy@naver.com>
2025-07-04 16:48:30 +0900

    Docs: 메모 삭제 API 명세 작성

--- src/swagger/notes.swagger.js
+++ src/swagger/notes.swagger.js
@@ -36,6 +36,10 @@
  *           description: 메모 내용
  *         created:
  *           type: Date
+ *           example: 2025-07-01T10:45:22.000Z
+ *           description: 메모 생성 일시
+ *         updated:
+ *           type: Date
  *           example: 2025-07-04T10:45:22.000Z
  *           description: 메모 생성 일시
  *     NoteRequestModel:
@@ -117,6 +121,18 @@
  *                     message:
  *                       type: string
  *                       example: id 형식이 올바르지 않습니다.
+ *       404:
+ *         description: 데이터 조회 실패
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: 해당 메모를 찾을 수 없습니다.
  *       500:
  *         description: 서버 에러
  *         content:
@@ -167,6 +183,139 @@
  *                     message:
  *                       type: string
  *                       example: 필수 데이터를 입력하세요.
+ *       500:
+ *         description: 서버 에러
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: Internal Server Error
+ *
+ * @openapi
+ * /notes/{uuid}:
+ *   put:
+ *     summary: 메모 수정
+ *     description: 메모 상세 정보를 조회합니다.
+ *     tags: [Notes]
+ *     parameters:
+ *       - in: path
+ *         name: uuid
+ *         type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/NoteRequestModel'
+ *     responses:
+ *       200:
+ *         description: 메모 수정 결과
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseSuccessModel'
+ *                 - type: object
+ *                   properties:
+ *                     item:
+ *                       $ref: '#/components/schemas/NoteResponseModel'
+ *       400:
+ *         description: Client 요청 에러
+ *         content:
+ *           application/json:
+ *             schema:
+ *               oneOf:
+ *                 - allOf:
+ *                   - $ref: '#/components/schemas/ResponseErrorModel'
+ *                   - type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         example: id 형식이 올바르지 않습니다.
+ *                 - allOf:
+ *                   - $ref: '#/components/schemas/ResponseErrorModel'
+ *                   - type: object
+ *                     properties:
+ *                       message:
+ *                         type: string
+ *                         example: 필수 데이터를 입력하세요.
+ *       404:
+ *         description: 데이터 조회 실패
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: 해당 메모를 찾을 수 없습니다.
+ *       500:
+ *         description: 서버 에러
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: Internal Server Error
+ *
+ * @openapi
+ * /notes/{uuid}:
+ *   delete:
+ *     summary: 메모 삭제
+ *     description: 메모를 삭제합니다.
+ *     tags: [Notes]
+ *     parameters:
+ *       - in: path
+ *         name: uuid
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: 삭제 완료
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseSuccessModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: 메모가 삭제되었습니다.
+ *       400:
+ *         description: path 값 에러
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: id 형식이 올바르지 않습니다.
+ *       404:
+ *         description: 데이터 조회 실패
+ *         content:
+ *           application/json:
+ *             schema:
+ *               allOf:
+ *                 - $ref: '#/components/schemas/ResponseErrorModel'
+ *                 - type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: 해당 메모를 찾을 수 없습니다.
  *       500:
  *         description: 서버 에러
  *         content:
